6ebe87f1d15a6bdeaf248d923b925457669ea2fe
OnTheWay0103 <OnTheWay0103@163.com>
2024-06-25 11:45:25 +0800

    auto commit update file

---
+++ daily_log/update_my_github.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+#同步github /Users/zhaoq0103/DEV/Personal/How-to-Make-Money/daily_log/update_my_github.sh
+# 这个脚本由 cron 上午9点，下午6点 定期执行 MAC OS
+# crontab -e -l -r 
+# man crontab 
+
+
+pushd ~/DEV/Personal/How-to-Make-Money && git pull 
+popd 
+
+pushd ~
+	log_file="github_update_log"
+
+	if [ ! -f "$log_file" ]; then
+	    touch "$log_file"
+	    echo "File created: $log_file"
+	else
+	    echo "pushd ~/DEV/Personal/How-to-Make-Money && git pull . time:""$(date)"  >> "$log_file"
+	    echo "log written to $log_file"
+	fi
+popd 
+
+
+
+
+#更新github， 把更新推上去
